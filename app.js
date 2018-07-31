71a1c43facb8623656012fde0b0e2bca4b4dceab
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-01 02:47:09 +0300

    js_language_basics_3.3: There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays and save them to third array

--- app.js
+++ app.js
@@ -8,7 +8,28 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        let arr1 = [1, 2, 3, 4, 5, 6];
+        let arr2 = [1, 2, 3, 4, 5];
+        let thirdArray = [];
+
+        let maxLenght = arr1.length;
+
+        if (maxLenght < arr2.length) {
+			maxLenght = arr2.length
+        }
+
+        for (let i = 0; i < maxLenght; i++) {
+        	let sum = arr1[i];
+        	if (arr1[i] && arr2[i]) {
+        		sum = arr1[i] + arr2[i];
+        	} else {
+        		sum = arr1[i] || arr2[i];
+        	}
+        	thirdArray.push(sum);
+        }
+
+        console.log(thirdArray);
     }   
 });
