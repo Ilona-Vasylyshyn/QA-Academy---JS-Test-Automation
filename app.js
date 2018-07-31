ac3c627bc7c5c6878e9b322807a4faa5bc0d215a
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-01 01:53:32 +0300

    js_language_basics_1.5: Write a JS program to find if the first number is larger from the two given positive integers

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        let firstInteger = 123;
+        let secondInteger = 122;
+
+        if (firstInteger > secondInteger) {
+        	console.log('first integer is larger');
+        } else {
+        	console.log('second integer is larger');
+        }
     }   
 });
