f9501bb97f6b4d37893819a3cb0175544a069304
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-01 01:36:45 +0300

    js_language_basics_1.1: Write a JS program to compute and print the sum of the two given integers

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        let a = 1;
+        let b = 2;
+        let sum = a + b;
+
+        console.log(sum);
     }   
 });
