13d918373618e66e406e23417f745c457a8eb51f
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-01 02:39:26 +0300

    js_language_basics_3.2: Write a simple JavaScript program to join all elements of the following array into a string

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        const arr = [1,2,3,4,5,6];
+        const result = arr.join();
+
+        console.log(result);
     }   
 });
