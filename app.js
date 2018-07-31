d65b939255171eb0a9bab0afadf448f5c09c799b
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-01 01:57:45 +0300

    js_language_basics_1.6: Write a JS program to check two given numbers and return true if one of the number is 50

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        let firstInteger = 123;
+        let secondInteger = 122;
+
+        if (firstInteger === 50 || (firstInteger + secondInteger) === 50) {
+        	console.log('THe first given integer is 50 or the sum of two elements is 50');
+        }
     }   
 });
