ca1313203e1a06b968b046b87fa5f9bd7a5cb020
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-01 02:27:14 +0300

    js_language_basics_2.5: Write a JavaScript function with conditional statement to sort three numbers

--- app.js
+++ app.js
@@ -8,7 +8,33 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+
+        function sortThreeNumbers (a, b, c) {
+        	if (a > b) {
+        		if (b > c) {
+        			console.log(a, b, c);
+        		} else {
+					if (a > c) {
+						console.log(a, c, b);
+					} else {
+						console.log(c, a, b);
+					}
+        		}
+        	} else {
+				if (a > c) {
+        			console.log(b, a, c);
+        		} else {
+					if (b > c) {
+						console.log(b, c, a);
+					} else {
+						console.log(c, b, a);
+					}
+        		}
+        	}
+        }
+
+        sortThreeNumbers(5, 6, 7);
     }   
 });
