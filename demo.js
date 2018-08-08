1ce40f240fb3eaec70b82acaea5f997d94e11921
Ilona Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-08 22:51:08 +0300

    js_EC6_OOP_1: Override method of father’s class.

--- demo.js
+++ demo.js
@@ -24,6 +24,14 @@ class Car extends Vehicle {
 		console.log(`I am starting transporting passengers`);
 	}
 	
+	displayInfo() {
+		console.log(`
+		Overridden father's class
+		Name: ${this.model};
+		Year: ${this.year};
+		Max Speed: ${this.maxSpeed};
+		Type ${this.type};`);
+	}
 	static colorRoof() {
 		console.log('The method colorRoof has been called');
 	}
