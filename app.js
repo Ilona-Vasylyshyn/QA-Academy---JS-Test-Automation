72e77a7e7fca53c8d1546f2199657c2a08767e72
Ilona Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-08 22:31:12 +0300

    js_EC6_OOP_1: small changes

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const {Vehicle, Car, Truck} = require('./demo');
-
 console.log("\nNice work! Your first application have been launched.");
 console.log("Now, I need to know your name, type it below: ");
 
@@ -12,14 +10,5 @@ stdin.addListener("data", (txt) => {
     } else {
         console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
         console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-		
-		const car = new Car('BMW', 2015, 310);
-		const truck = new Truck('Renault', 2012, 180);
-		
-		car.transportPeople();
-		truck.transportContainer();
-		
-		car.displayInfo();
-		truck.displayInfo();
-	}   
+    }   
 });
