ecd36a323bc0430300a5abf78c39d35fc02f2f55
Ilona Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-08 23:26:44 +0300

    js_async_await_3: repeat work done on slide 11

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+		Promise.all([
+			new Promise((resolve, reject) => setTimeout(() => resolve(1), 3000)), // 1
+			new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)), // 2
+			new Promise((resolve, reject) => setTimeout(() => resolve(3), 1000)), // 3
+		]).then((results) => {
+			console.log(results[0]);
+			console.log(results[2]);
+		});
     }   
 });
