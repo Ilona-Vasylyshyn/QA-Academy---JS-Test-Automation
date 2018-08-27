ddd834ced32911a5da679ad10b9d5aae5aa3a6a3
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-27 23:17:28 +0300

    js_extra_packages_json_3: Use JSON.stringify() method to make a JSON string

--- app.js
+++ app.js
@@ -11,5 +11,6 @@ stdin.addListener("data", (txt) => {
         //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
         //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
         const student = {name: 'Ilona', surname: 'Vasylyshyn', rate: 99};
+        const stringify = JSON.stringify(student);
     }   
 });
