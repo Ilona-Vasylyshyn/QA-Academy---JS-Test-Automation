d636c0acfff6060e41eca9add49100cc2ea7c4c5
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-27 23:33:45 +0300

    js_extra_packages_random_2: create js program which will provide you a number with a defined length

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ stdin.addListener("data", (txt) => {
         console.log("\nHave a nice JS trip! Bye-bye");
         stdin.end();
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
+        //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
+        const definedLength = 10;
+        const result = Math.floor((Math.random() * Math.pow(10, definedLength)) + 1);
+
+        console.log('random number', result);
     }   
 });
