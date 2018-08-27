149de1c7bbde0b49d852798c2000c4f1b3d73527
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-27 23:46:33 +0300

    js_extra_packages_file_system_3: update created file using writeFile()

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ stdin.addListener("data", (txt) => {
     } else {
         //console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
         //console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-        fs.appendFile('myfile.txt', 'file updated', function (err, file) {
+        fs.writeFile('myfile.txt', 'file has been written', function (err, file) {
         	if (err) {
         		console.log(err);
         		return;
