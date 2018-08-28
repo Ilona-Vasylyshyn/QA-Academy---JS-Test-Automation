27ad3851982121d09f1bd6959ccb3a81f7c083cc
Ilona.Vasylyshyn <ilona.vasylyshyn@gmail.com>
2018-08-28 21:55:32 +0300

    js_jasmine_2: create a suite to verify if JS program works correctly

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,9 +1,25 @@
 function addTwoIntegers (a, b) {
-	return a + b;
+	return parseInt(a, 10) + parseInt(b, 10);
 }
 
-describe('Add two integers', function() {
-	it ('correct adding integers', function(){
+describe('Add two integers:', function() {
+	it ('correct adding integers', function() {
 		expect(addTwoIntegers(10, 20)).toBe(30);
+	});
+
+	it ('incorrect adding integer and string', function() {
+		expect(addTwoIntegers(10, 'a')).toBeNaN();
+	});
+
+	it ('correct adding integer and number in string', function() {
+		expect(addTwoIntegers(10, '11')).toBe(21);
+	});
+
+	it ('correct adding integer and float', function() {
+		expect(addTwoIntegers(10, 10.1)).toBe(20);
+	});
+
+	it ('correct adding positive and negative integers', function() {
+		expect(addTwoIntegers(10, -5)).toBe(5);
 	});
 });
